372afe8afdc0e0acaeb224a04da802c80da25f00
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-02-09 22:33:51 -0500

    converted connection to es6

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/tech-friends',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
+	process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/hand-me-up",
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
 );
 
-module.exports = mongoose.connection;
+export default mongoose.connection;

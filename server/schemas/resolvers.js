8e4d44a119a86ac2c4741c45342a34b31b5117be
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-02-09 22:47:48 -0500

    convert resolvers to es6

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,57 +1,57 @@
-const { GraphQLError } = require('graphql');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+import { GraphQLError } from "graphql";
+import { User } from "../models";
+import { signToken } from "../utils/auth";
 
 const resolvers = {
-  Query: {
-    users: async () => {
-      return User.find();
-    },
+	Query: {
+		users: async () => {
+			return User.find();
+		},
 
-    user: async (parent, { userId }) => {
-      return User.findOne({ _id: userId });
-    },
-    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
-    me: async (parent, args, context) => {
-      if (context.user) {
-        return User.findOne({ _id: context.user._id });
-      }
-      throw new GraphQLError('You need to be logged in!');
-    },
-  },
+		user: async (parent, { userId }) => {
+			return User.findOne({ _id: userId });
+		},
+		// By adding context to our query, we can retrieve the logged in user without specifically searching for them
+		me: async (parent, args, context) => {
+			if (context.user) {
+				return User.findOne({ _id: context.user._id });
+			}
+			throw new GraphQLError("You need to be logged in!");
+		},
+	},
 
-  Mutation: {
-    addUser: async (parent, { name, email, password }) => {
-      const user = await User.create({ name, email, password });
-      const token = signToken(user);
+	Mutation: {
+		addUser: async (parent, { name, email, password }) => {
+			const user = await User.create({ name, email, password });
+			const token = signToken(user);
 
-      return { token, user };
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
+			return { token, user };
+		},
+		login: async (parent, { email, password }) => {
+			const user = await User.findOne({ email });
 
-      if (!user) {
-        throw new GraphQLError('No user with this email found!');
-      }
+			if (!user) {
+				throw new GraphQLError("No user with this email found!");
+			}
 
-      const correctPw = await user.isCorrectPassword(password);
+			const correctPw = await user.isCorrectPassword(password);
 
-      if (!correctPw) {
-        throw new GraphQLError('Incorrect password!');
-      }
+			if (!correctPw) {
+				throw new GraphQLError("Incorrect password!");
+			}
 
-      const token = signToken(user);
-      return { token, user };
-    },
+			const token = signToken(user);
+			return { token, user };
+		},
 
-    // Set up mutation so a logged in user can only remove their user and no one else's
-    removeUser: async (parent, args, context) => {
-      if (context.user) {
-        return User.findOneAndDelete({ _id: context.user._id });
-      }
-      throw new GraphQLError('You need to be logged in!');
-    },
-  },
+		// Set up mutation so a logged in user can only remove their user and no one else's
+		removeUser: async (parent, args, context) => {
+			if (context.user) {
+				return User.findOneAndDelete({ _id: context.user._id });
+			}
+			throw new GraphQLError("You need to be logged in!");
+		},
+	},
 };
 
-module.exports = resolvers;
+export default resolvers;

2cc9ae84b29b8361f83e927a2dbab79917446b85
Eric Simmons <erictomlinsonsimmons@gmail.com>
2023-02-09 20:28:24 -0600

    update resolvers

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,8 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const profileSchema = new Schema({
-  name: {
+const userSchema = new Schema({
+  username: {
     type: String,
     required: true,
     unique: true,
@@ -19,16 +19,10 @@ const profileSchema = new Schema({
     required: true,
     minlength: 5,
   },
-  skills: [
-    {
-      type: String,
-      trim: true,
-    },
-  ],
 });
 
 // set up pre-save middleware to create password
-profileSchema.pre('save', async function (next) {
+userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
@@ -38,10 +32,10 @@ profileSchema.pre('save', async function (next) {
 });
 
 // compare the incoming password with the hashed password
-profileSchema.methods.isCorrectPassword = async function (password) {
+userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-const Profile = model('Profile', profileSchema);
+const User = model('User', userSchema);
 
-module.exports = Profile;
+module.exports = User;

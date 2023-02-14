a7b3d30869ae34bb93b78f4107bde1ea712e8a8c
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-02-13 22:56:49 -0500

    updated index.js with all models

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,10 @@
-import User from "User";
+import Address from "./Address";
+import Cart from "./Cart";
+import Category from "./Category";
+import Listing from "./Listing";
+import Order from "./Order";
+import Payment from "./Payment";
+import Tag from "./Tag";
+import User from "./User";
 
-export { User };
+export { Address, Cart, Category, Listing, Order, Payment, Tag, User };

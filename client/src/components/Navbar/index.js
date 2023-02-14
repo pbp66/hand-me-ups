d0372d06a14f8b967796c084a8dc4b2336c5debc
Cody <110630101+CodySaal@users.noreply.github.com>
2023-02-13 20:36:24 -0600

    Adjusted active links on navbar and used react-router-dom

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,29 +1,31 @@
+import { NavLink } from "react-router-dom"
+
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Hand Me Ups</a>
+                <NavLink className="navbar-brand" to="/">Hand Me Ups</NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/Login">Login</a>
+                            <NavLink className="nav-link" aria-current="page" to="/Login">Login</NavLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/ShoppingCart">Cart</a>
+                            <NavLink className="nav-link" to="/ShoppingCart">Cart</NavLink>
                         </li>
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Account
                             </a>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="/SavedItems">Saved Items</a></li>
-                                <li><a className="dropdown-item" href="/MyListings">My Listings</a></li>
-                                <li><a className="dropdown-item" href="/PurchaseHistory">Purchase History</a></li>
+                                <li><NavLink className="dropdown-item" to="/SavedItems">Saved Items</NavLink></li>
+                                <li><NavLink className="dropdown-item" to="/MyListings">My Listings</NavLink></li>
+                                <li><NavLink className="dropdown-item" to="/PurchaseHistory">Purchase History</NavLink></li>
                                 <li><hr className="dropdown-divider" /></li>
-                                <li><a className="dropdown-item" href="/">Logout</a></li>
+                                <li><btn className="dropdown-item">Logout</btn></li>
                             </ul>
                         </li>
                     </ul>

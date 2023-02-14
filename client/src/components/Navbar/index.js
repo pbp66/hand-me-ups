63c9928aaf93c819372e11b99c52796f553160c2
Cody <110630101+CodySaal@users.noreply.github.com>
2023-02-13 20:43:51 -0600

    Formatting Search form

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -29,8 +29,8 @@ const Navbar = () => {
                             </ul>
                         </li>
                     </ul>
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                    <form className="d-flex justify-content-end" role="search">
+                        <input className="form-control me-2 w-50" type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-outline-success" type="submit">Search</button>
                     </form>
                 </div>

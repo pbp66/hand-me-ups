3434d7fc9e01c9e445d60c1b25acbfd9439d9a7a
Julissa Bonilla <julissab9@gmail.com>
2023-02-13 20:53:10 -0600

    started working on styled components

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,6 +2,36 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import Auth from "../../utils/auth";
+import styled from 'styled-components';
+import { colors } from '../utils/constants';
+
+const StyledLink = styled.a`
+  background-color: ${colors.celeste};
+  color: white;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #B1F8F2;
+  border-radius: 3px;
+  display: block; 
+  &:hover {
+        background: ${colors.mindaro};
+    };
+`
+const StyleHeader = styled.header`
+  display: flex;
+  align: center;
+  background-color: ;
+  
+`
+const StyledDiv = styled.div`
+  display: flex-column;
+  align: center;
+  text: center;
+  justify: space-between-lg;
+  justify: center;
+`
+
 
 const Header = () => {
 	const logout = (event) => {
@@ -9,9 +39,9 @@ const Header = () => {
 		Auth.logout();
 	};
 	return (
-		<header className="bg-info text-dark mb-4 py-3 display-flex align-center">
-			<div className="container flex-column justify-space-between-lg justify-center align-center text-center">
-				<Link
+		<StyleHeader className="bg-info text-dark mb-4 py-3">
+			<StyledDiv className="container">
+				<StyledLink
 					className="text-dark"
 					to="/"
 				>
@@ -21,7 +51,7 @@ const Header = () => {
 					>
 						Tech Friends
 					</h1>
-				</Link>
+				</StyledLink>
 				<p
 					className="m-0"
 					style={{ fontSize: "1.75rem", fontWeight: "700" }}
@@ -31,12 +61,12 @@ const Header = () => {
 				<div>
 					{Auth.loggedIn() ? (
 						<>
-							<Link
+							<StyledLink
 								className="btn btn-lg btn-primary m-2"
 								to="/me"
 							>
 								View My User
-							</Link>
+							</StyledLink>
 							<button
 								className="btn btn-lg btn-light m-2"
 								onClick={logout}
@@ -46,23 +76,23 @@ const Header = () => {
 						</>
 					) : (
 						<>
-							<Link
+							<StyledLink
 								className="btn btn-lg btn-primary m-2"
 								to="/login"
 							>
 								Login
-							</Link>
-							<Link
+							</StyledLink>
+							<StyledLink
 								className="btn btn-lg btn-light m-2"
 								to="/signup"
 							>
 								Signup
-							</Link>
+							</StyledLink>
 						</>
 					)}
 				</div>
-			</div>
-		</header>
+			</StyledDiv>
+		</StyleHeader>
 	);
 };
 
---
+++ client/src/utils/constants.js
@@ -0,0 +1,9 @@
+export const colors = {
+    celeste : '#B1F8F2',
+    celadon : '#BCD39C',
+    mindaro: '#FFFC99',
+    nyzanza: '#EAFDCF',
+    moss: '#EAFDCF',
+    cream: '#F5FDB4',
+    
+ }

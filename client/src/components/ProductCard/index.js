414e1153b9a57ba88f8278753e74572016467432
adriennecordell <adrienne.cordellac@gmail.com>
2023-02-13 20:00:07 -0600

    added product card to components

---
+++ client/src/components/ProductCard/index.js
@@ -0,0 +1,23 @@
+import react from 'react'
+
+function ProductCard ({ product }) {
+    const title = product.title
+    const description = product.description
+    const price = product.price
+    const image = product.img
+    return (
+        <>
+        <div>
+            {title}
+        </div>
+        <div>
+            {description}
+        </div>
+        <div>
+            {price}
+        </div>
+        </>
+    )
+}
+
+export default ProductCard

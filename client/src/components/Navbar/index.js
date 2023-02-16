b69752cf2213af4d907a201b22f598b25a7cd90a
Julissa Bonilla <julissab9@gmail.com>
2023-02-15 19:59:58 -0600

    working through styles

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,31 +1,48 @@
 import { NavLink } from "react-router-dom"
+import styled from 'styled-components'
+
+const Title = styled.nav`
+    color: #414535;
+`
+const MainDiv = styled.div`
+    background-color: #F7FFF6;
+`
+const NavTitle = styled.nav`
+    color: #618985;
+    &:hover {
+        color: #96BBBB;
+    }
+`
+const SearchForm = styled.form `
+
+`
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <NavLink className="navbar-brand" to="/">Hand Me Ups</NavLink>
+            <MainDiv className="container-fluid">
+                <NavLink className="navbar-brand" to="/"><Title>Hand Me Ups</Title></NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <NavLink className="nav-link" aria-current="page" to="/Login">Login</NavLink>
+                            <NavLink className="nav-link" aria-current="page" to="/Login"><NavTitle>Login</NavTitle></NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="/ShoppingCart">Cart</NavLink>
+                            <NavLink className="nav-link" to="/ShoppingCart"><NavTitle>Cart</NavTitle></NavLink>
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Account
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><NavTitle>
+                                Account</NavTitle>
                             </a>
                             <ul className="dropdown-menu">
-                                <li><NavLink className="dropdown-item" to="/SavedItems">Saved Items</NavLink></li>
-                                <li><NavLink className="dropdown-item" to="/MyListings">My Listings</NavLink></li>
-                                <li><NavLink className="dropdown-item" to="/PurchaseHistory">Purchase History</NavLink></li>
+                                <li><NavLink className="dropdown-item" to="/SavedItems"><NavTitle>Saved Items</NavTitle></NavLink></li>
+                                <li><NavLink className="dropdown-item" to="/MyListings"><NavTitle>My Listings</NavTitle></NavLink></li>
+                                <li><NavLink className="dropdown-item" to="/PurchaseHistory"><NavTitle>Purchase History</NavTitle></NavLink></li>
                                 <li><hr className="dropdown-divider" /></li>
-                                <li><button className="dropdown-item">Logout</button></li>
+                                <li><btn className="dropdown-item"><NavTitle>Logout</NavTitle></btn></li>
                             </ul>
                         </li>
                     </ul>
@@ -34,7 +51,7 @@ const Navbar = () => {
                         <button className="btn btn-outline-success" type="submit">Search</button>
                     </form>
                 </div>
-            </div>
+            </MainDiv>
         </nav>
 
     )
---
+++ client/src/utils/constants.js
@@ -0,0 +1,14 @@
+// export colors = {
+//    lion #C19875
+//    dutchWhite #F2E3BC
+//     blackolive #414535
+//    hookersGreen #618985
+//   ashgray  #96BBBB
+// coolgrey #8491A3
+//mint cream #F7FFF6
+//lavendar blush #F8E9E9
+// tan  #CBAC88
+//outerspace #394648
+
+
+// }

31e06d60812a5681d6cb2ed84fbfe70d4ecf3ce4
adriennecordell <adrienne.cordellac@gmail.com>
2023-02-16 20:46:10 -0600

    removed link

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../Navbar"
-import image from "../../assets/retro-header.png"
+// import image from "../../assets/retro-header.png"
 
 import Auth from "../../utils/auth";
 
@@ -11,7 +11,7 @@ const Header = () => {
 		Auth.logout();
 	};
 	return (
-		<Navbar style={{ backgroundImage: `url(${image})` }}/>
+		<Navbar />
 		// <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
 		// 	<div className="container flex-column justify-space-between-lg justify-center align-center text-center">
 		// 		<Link

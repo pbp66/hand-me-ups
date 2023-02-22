4e6933e96cfeca0991ad1a83455ec212dd34f094
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-02-21 20:04:09 -0500

    added todos for remaining queries. changed order of queries to match order in typeDefs

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -30,6 +30,54 @@ export const QUERY_ME = gql`
 	}
 `;
 
+export const QUERY_LISTINGS = gql`
+	query QUERY_LISTINGS {
+		allListings {
+			_id
+			title
+			description
+			price
+			category {
+				_id
+			}
+			tags {
+				_id
+			}
+			size
+			color
+			condition
+			image
+			seller {
+				_id
+			}
+			listing_date
+			edit_status
+			edit_dates
+		}
+	}
+`;
+
+export const QUERY_ONE_LISTING = gql`
+	query QUERY_ONE_LISTING($listingId: ID!) {
+		oneListing(listingId: $listingId) {
+			_id
+			title
+			description
+			price
+			size
+			color
+			condition
+			image
+			seller {
+				_id
+			}
+			listing_date
+			edit_status
+			edit_dates
+		}
+	}
+`;
+
 export const QUERY_CHECKOUT = gql`
 	query getCheckout($products: [ID]!) {
 		checkout(products: $products) {
@@ -38,33 +86,72 @@ export const QUERY_CHECKOUT = gql`
 	}
 `;
 
-export const QUERY_LISTINGS = gql`
-query QUERY_LISTINGS {
-	allListings {
-	  _id
-	  title
-	  description
-	  price
-	  category {
-		_id
-	  }
-	  tags {
-		_id
-	  }
-	  size
-	  color
-	  condition
-	  image
-	  seller {
-		_id
-	  }
-	  listing_date
-	  edit_status
-	  edit_dates
+export const QUERY_USER_LISTINGS = gql`
+	query QUERY_USER_LISTINGS($userId: ID!) {
+		userListings(userId: $userId) {
+			_id
+			title
+			description
+			price
+			category {
+				_id
+				category
+			}
+			tags {
+				_id
+				tag
+			}
+			size
+			color
+			condition
+			image
+			seller {
+				_id
+			}
+			listing_date
+			edit_status
+			edit_dates
+		}
 	}
-  }
-  
 `;
+
+export const QUERY_MY_LISTINGS = gql`
+	query QUERY_MY_LISTINGS {
+		myListings {
+			_id
+			title
+			description
+			price
+			category {
+				category
+			}
+			tags {
+				tag
+			}
+			seller {
+				_id
+			}
+			size
+			color
+			condition
+			image
+			listing_date
+			edit_status
+			edit_dates
+		}
+	}
+`;
+// TODO FAVORITE_LISTINGS
+
+// TODO SEARCH_LISTINGS
+
+// TODO ALL_ORDERS
+
+// TODO GET_ORDER
+
+// TODO MY_ORDER
+
+// TODO ALL_TAGS
 
 export const QUERY_ALL_CATEGORIES = gql`
 	query ALL_CATEGORIES {
@@ -75,97 +162,23 @@ export const QUERY_ALL_CATEGORIES = gql`
 	}
 `;
 
+// TODO MY_PAYMENT_METHODS
+
+// TODO MY_ADDRESSES
+
 export const QUERY_MY_CART = gql`
-query MY_CART{
-	myCart {
-	  _id
-	  items {
-		_id
-		title
-		price
-		image
-		seller {
-		  _id
+	query MY_CART {
+		myCart {
+			_id
+			items {
+				_id
+				title
+				price
+				image
+				seller {
+					_id
+				}
+			}
 		}
-	  }
 	}
-  }`
-
-export const QUERY_USER_LISTINGS = gql`
-  query QUERY_USER_LISTINGS($userId: ID!) {
-	userListings(userId: $userId) {
-	  _id
-	  title
-	  description
-	  price
-	  category {
-		_id
-		category
-	  }
-	  tags {
-		_id
-		tag
-	  }
-	  size
-	  color
-	  condition
-	  image
-	  seller {
-		_id
-	  }
-	  listing_date
-	  edit_status
-	  edit_dates
-	}
-  }
-  `
-
-export const QUERY_MY_LISTINGS = gql`
-  query QUERY_MY_LISTINGS {
-    myListings {
-      _id
-      title
-      description
-      price
-      category {
-        category
-      }
-      tags {
-        tag
-      }
-	  seller {
-		_id
-	  }
-      size
-      color
-      condition
-      image
-      listing_date
-      edit_status
-      edit_dates
-    }
-  }
-  `
-export const QUERY_ONE_LISTING = gql`	
-query QUERY_ONE_LISTING($listingId: ID!) {
-	oneListing(listingId: $listingId) {
-	  _id
-	  title
-	  description
-	  price
-	  size
-	  color
-	  condition
-	  image
-	  seller {
-		_id
-	  }
-	  listing_date
-	  edit_status
-	  edit_dates
-	}
-  }
-`
-
-
-
+`;

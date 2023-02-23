f046e6607c2edf7483e091d6b468e6c7d846e050
Cody <110630101+CodySaal@users.noreply.github.com>
2023-02-22 22:51:23 -0600

    Working on removing favorites

--- client/src/pages/Favorites.js
+++ client/src/pages/Favorites.js
@@ -1,32 +1,68 @@
 import { useQuery } from '@apollo/client'
+import { useMutation } from '@apollo/client';
+import { Button, Container, Row, Col } from "react-bootstrap";
 import Grid from "../components/Grid"
 import Listing from "../components/Listing"
+import { UNFAVORITE_LISTING } from '../utils/mutations';
 import { QUERY_FAVORITE_LISTINGS } from '../utils/queries';
 
 
 
 
 
-const Favorites = () => {
+
+const Favorites = (props) => {
 	const { data, loading, error } = useQuery(QUERY_FAVORITE_LISTINGS);
 	console.log(data)
 	const myFavorites = data?.favoriteListings || [];
+
+	const [unFavoriteListing] = useMutation(UNFAVORITE_LISTING, {
+		refetchQueries: [
+			{
+				query:
+					QUERY_FAVORITE_LISTINGS
+			},
+			"FAVORITE_LISTINGS",
+		]
+	})
+
+	const handleFavorite = async (id) => {
+		console.log(id)
+		await unFavoriteListing({
+			variables: { listingId: id },
+		})
+	};
 
 
 
 	return (
 		<>
 			<Grid colCount={4} md={3}>
-			{myFavorites.map(listing => {
-				return (<>
-					<Listing
-						key={listing._id}
-						listing={listing}
-					>
-					</Listing>
-				</>)
-			})}
-		</Grid>
+				{myFavorites.map(listing => {
+					return (<>
+						<Listing
+							key={listing._id}
+							listing={listing}
+						>
+						</Listing>
+
+						<Container>
+							<Row>
+								<Col>
+									<Button
+										onClick={() => {
+											handleFavorite(listing._id);
+										}}
+									>
+										UnFavorite
+									</Button>
+								</Col>
+							</Row>
+						</Container>
+					</>)
+				})}
+			</Grid>
+
 		</>
 	)
 }

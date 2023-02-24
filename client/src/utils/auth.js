d0eebfc558ddde26a54c59663109b9e2c42eadcc
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-02-23 19:49:23 -0500

    fixed log in bug

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -25,9 +25,10 @@ class AuthService {
 
 	login(idToken) {
 		localStorage.setItem("id_token", idToken);
-		const decoded = decode(idToken)
-		const {_id} = decoded?.data
-		window.location.assign(`/MyListings/${_id}`);
+		const decoded = decode(idToken);
+		const { _id } = decoded?.data;
+		//window.location.assign(`/MyListings/{_id}`)
+		window.location.assign(`/`);
 	}
 
 	logout() {

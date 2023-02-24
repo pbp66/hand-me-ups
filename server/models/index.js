0d2289e964b54d6937b4624bf9ce3e37b37d89e1
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-02-23 20:42:36 -0500

    fixed model imports for heroku

--- server/models/index.js
+++ server/models/index.js
@@ -1,10 +1,10 @@
-import Address from "./Address";
-import Cart from "./Cart";
-import Category from "./Category";
-import Listing from "./Listing";
-import Order from "./Order";
-import Payment from "./Payment";
-import Tag from "./Tag";
-import User from "./User";
+import Address from "./Address.js";
+import Cart from "./Cart.js";
+import Category from "./Category.js";
+import Listing from "./Listing.js";
+import Order from "./Order.js";
+import Payment from "./Payment.js";
+import Tag from "./Tag.js";
+import User from "./User.js";
 
 export { Address, Cart, Category, Listing, Order, Payment, Tag, User };
